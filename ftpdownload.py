86769c59206f87b26bf030c22a646aee41847b3e
Fabian Jankowski <fjankowsk@gmail.com>
2020-10-14 12:38:42 +0100

    Switch to using argparse for the commandline option parsing and make a bit nicer.

--- ftpdownload.py
+++ ftpdownload.py
@@ -2,28 +2,41 @@
 '''
 Script to download publicly available TEC maps
 usage:
-python ftpdownload.py -d yyyy-mm-dd [-t type]
--d gives date of observation
+python ftpdownload.py yyyy-mm-dd [-t type]
+The positional argument gives the date of the observation
 -t gives type of ionex file (codg,upcg,igsg) codg maps are default option
 
 v0.1 Charlotte Sobey 2013 
 '''
 
+import argparse
 import ftplib
 import datetime
 import optparse as op
 
-p = op.OptionParser()
-p.add_option('--date','-d',default='NONE',type='string',help='Date (yyyy-mm-dd)')
-p.add_option('--type','-t',default='codg',type='string',help='Type of ionex file (codg,upcg,igsg) [codg default]')
-ops,args = p.parse_args()
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+        'date',
+        type=str,
+        help='Date (yyyy-mm-dd)'
+)
+
+parser.add_argument(
+        '--type', '-t',
+        default='codg',
+        type=str,
+        help='Type of ionex file (codg,upcg,igsg) [codg default]'
+)
+
+args = parser.parse_args()
 
 # Reading the date provided
 #date = raw_input('date of observation?(yyyy-mm-dd): ')
 #filetype = raw_input('Type of ionex file?(codg,upcg,igsg): ')
-year = int(ops.date.split('-')[0])
-month = int(ops.date.split('-')[1])
-day = int(ops.date.split('-')[2])
+year = int(args.date.split('-')[0])
+month = int(args.date.split('-')[1])
+day = int(args.date.split('-')[2])
 
 dayofyear = datetime.datetime.strptime(''+str(year)+' '+str(month)+' '+str(day)+'', '%Y %m %d').timetuple().tm_yday
 
@@ -33,7 +46,7 @@ if dayofyear < 100 and dayofyear >= 10:
         dayofyear = '0'+str(dayofyear)
 
 # Outputing the name of the IONEX file you require
-file = str(ops.type)+str(dayofyear)+'0.'+str(list(str(year))[2])+str(list(str(year))[3])+'i.Z'
+file = str(args.type)+str(dayofyear)+'0.'+str(list(str(year))[2])+str(list(str(year))[3])+'i.Z'
 print('FILE:', file)
 directory = '/pub/gps/products/ionex/'+str(year)+'/'+str(dayofyear)+'/'
 print('DIR:', directory)
